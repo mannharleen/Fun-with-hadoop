b02dbcb51847f6c9e2e65f364cc0296a28e435a2
Harleen Mann <mannharleen@gmail.com>
2017-09-24 22:58:48 +0800

    Update partitioning_n_bucketing_basics.sql
    added insert into partitioned table, without using dynamic partitioning.

--- hive/partitioning_n_bucketing_basics.sql
+++ hive/partitioning_n_bucketing_basics.sql
@@ -4,29 +4,45 @@
 --lets see our current db on the commandline
 set hive.cli.print.current.db=true;
 use retail_db;
+
 --create a managed table 
 create table categories (cat_id int, cat_dept_id int, cat_name string) row format delimited fields terminated by ',';
+
 --load data from local file system
 load data local inpath '/home/cloudera/data/retail_db/categories/' into table categories;
 
+---------------------------------------------------------------------------------
 --create a partitioned table
 --not that Iam NOT including the partitioned column in the first bracket
 create table categories_partitionedby_deptid (cat_id int,cat_name string) partitioned by (cat_dept_id int);
+
 --must set to use dynamic partitioned
 set hive.exec.dynamic.partition.mode=nonstrict;
---insert 
+
+--insert (dynamic partitions) 
+	-->> cat_dept_id must be the last column in the select statement
 insert into categories_partitionedby_deptid partition(cat_dept_id) 
 	select cat_id, cat_name, cat_dept_id from categories;
+
+--insert (not dynamic partitions) 
+	-->> notice that we did not select 'cat_dept_id' column
+insert into categories_partitionedby_deptid partition(cat_dept_id=11) 
+	select cat_id, cat_namefrom categories;
+	
 --insert values
 insert into categories_partitionedby_deptid partition(cat_dept_id) 
 	values (1,'one',11),(2,'two',22);
+
 --validate
 show partitions categories_partitionedby_cat_dept_id;
 
+---------------------------------------------------------------------------------
 --create bucketed table
 create table categories_bucketedby_deptid(cat_id int, cat_dept_id int, cat_name string) clustered by (cat_dept_id) into 3 buckets;
+
 --must be true
 set hive.enforce.bucketing = true;
+
 --insert
 insert into table categories_bucketedby_deptid 
 	select * from categories;
